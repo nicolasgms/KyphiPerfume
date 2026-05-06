fc7bc874fab5e2236134e2eac72c15a50c6a16e1
Nicolas Gomes <nicolas-gomes2010@hotmail.fr>
2026-05-07 00:11:56 +0200

    Feature: Alphabet navigation

--- models/db.js
+++ models/db.js
@@ -1,12 +1,29 @@
+/* const mysql = require('mysql2/promise');
+
+const pool = mysql.createPool({
+  host: 'localhost',
+  user: 'root',
+  password: 'root',
+  database: 'parfum',
+  port: 8889, // Port MAMP par défaut
+  waitForConnections: true,
+  connectionLimit: 10
+});
+
+module.exports = pool;
+ */
+
+
+
 const mysql = require('mysql2/promise');
 require('dotenv').config();
 
 const pool = mysql.createPool({
     host: process.env.MYSQLHOST || process.env.MYSQL_HOST || 'localhost',
     user: process.env.MYSQLUSER || process.env.MYSQL_USER || 'root',
-    password: process.env.MYSQLPASSWORD || process.env.MYSQL_PASSWORD || '',
+    password: process.env.MYSQLPASSWORD || process.env.MYSQL_PASSWORD || 'root',
     database: process.env.MYSQLDATABASE || process.env.MYSQL_DATABASE || 'parfum',
-    port: process.env.MYSQLPORT || process.env.MYSQL_PORT || 3306,
+    port: process.env.MYSQLPORT || process.env.MYSQL_PORT || 8889,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
